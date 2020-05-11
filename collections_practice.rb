5609b0ad2f2d38fdaa11c297c22611874b0021b8
Raveen K Sharma <raveen08@gmail.com>
2020-05-11 04:53:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,5 +48,15 @@ def sum_array(array)
   sum
 end
 
-
+def add_s(array)
+  return_array = []
+      array.each_with_index do |word,index|
+        if index != 1 
+          word = word + "s"
+        end
+        return_array << word 
+  end
+  
+  return_array
+end
   
